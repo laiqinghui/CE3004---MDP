9c1817e85792f6b973ad373ffbcc542da54d7e5e
Lai Qing Hui <qinghui1994@gmail.com>
2018-01-24 23:30:38 +0800

    Completed the backbone of I2C communication protocol between Pi and Arduino

---
+++ Arduino/i2c/i2c.ino
@@ -0,0 +1,91 @@
+//Import the library required
+#include <Wire.h>
+#define PI_PIN A0
+
+/*
+  I2C Pinouts
+  SDA -> A4
+  SCL -> A5
+*/
+
+//Slave Address for the Communication
+#define SLAVE_ADDRESS 0x04
+
+char buffer[50] = {0};
+
+void setup() {
+  
+  // initialize i2c as slave
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+  pinMode(PI_PIN, OUTPUT);
+  digitalWrite(PI_PIN, LOW);
+ 
+  
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+  
+}
+
+void loop() {
+  //Serial.println(digitalRead(A0));
+  //delay(2000);
+} // end loop
+
+void printArray(char arr[], int len){
+    //Start from one as first char is empty
+    for(int i = 1; i < len ; i++){
+        Serial.print(arr[i]);
+      }
+    Serial.println();
+    
+  }
+  
+// callback for received data
+void receiveData(int byteCount) {
+  int i = 0;
+  int len = Wire.available();
+  Serial.print("Incoming: ");
+  Serial.print(len);
+  Serial.println(" bytes");
+
+  
+  if(len > 1){
+    while (i<len) {
+      
+      buffer[i] = Wire.read();
+      
+      //Serial.print("buffer[i]: ");
+      //Serial.println(buffer[i]);
+      i++;
+  
+    }
+    
+    Serial.print("buffer: ");
+    printArray(buffer, len);
+    interruptPi();
+  } else {
+    
+      //Single ack byte return from the Pi after Arduino sent a string over
+      //Wire.read() here is required to clear off the the ack byte from the Wire Buffer in order to accept more new bytes
+      Serial.print("Ack byte: ");
+      Serial.println(Wire.read());
+    
+    }
+}  // end while
+
+void interruptPi(){
+    digitalWrite(PI_PIN, HIGH);
+    delay(500);
+    digitalWrite(PI_PIN, LOW);
+    
+  }
+
+// callback for sending data
+void sendData() {
+  Wire.write("Data From Arduino!");
+}
+
+
+
