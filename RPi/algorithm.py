4b663eefc8d415d2a60ee812ec792b5fd99a9321
sohjunjie <junjie.soh93@gmail.com>
2018-01-27 16:09:12 +0800

    add params for initializing robot position

--- RPi/algorithm.py
+++ RPi/algorithm.py
@@ -7,21 +7,22 @@ from pydispatch import dispatcher
 
 
 class Algorithm(threading.Thread):
-    def __init__(self):
+    def __init__(self, robot_x, robot_y):
 
         print "algorithm initialized"
 
         _x = 20
         _y = 15
         self.map = [[0 for i in range(_x)] for j in range(_y)]
-        self.robot_x = 0
-        self.robot_y = 0
+        self.robot_x = robot_x
+        self.robot_y = robot_y
 
         dispatcher.connect(self.determine_robot_path, signal=gs.RPI_ALGORITHM_SIGNAL, sender=gs.RPI_SENDER)
         self.idle()
 
     def determine_robot_path(self, message):
         # message e.g. front and side have obstacle or not
+        print "Robot now at position " + str(self.robot_x) + ", " + str(self.robot_y)
         print "Algorithm receive obstacle info: " + str(message) + ", now calculating robot path..."
         time.sleep(1)
         dispatcher.send(message='move robot', signal=gs.ALGORITHM_SIGNAL, sender=gs.ALGORITHM_SENDER)
--- RPi/main.py
+++ RPi/main.py
@@ -1,11 +1,13 @@
-import threading
+import getopt
 import time
+import threading
+import sys
+
+from pydispatch import dispatcher
 
 from algorithm import Algorithm
 
 import global_settings as gs
-
-from pydispatch import dispatcher
 
 
 class RPI(threading.Thread):
@@ -26,12 +28,36 @@ class RPI(threading.Thread):
             time.sleep(1)
 
 
+def main(argv):
+
+    robot_x = 0
+    robot_y = 0
+
+    try:
+        opts, remainders = getopt.getopt(argv, "x:y:", ["robot_x=", "robot_y="])
+    except getopt.GetoptError:
+        pass
+
+    for opt, arg in opts:
+
+        if opt in ("-x", "--robot_x"):
+            robot_x = int(arg)
+        elif opt in ("-y", "--robot_y"):
+            robot_y = int(arg)
+        else:
+            assert False, "unhandled option"
+
+    return robot_x, robot_y
+
+
 if __name__ == "__main__":
+
+    robot_x, robot_y = main(sys.argv[1:])
 
     gs.init()
 
     rpi_thread = threading.Thread(target=RPI)
-    algo_thread = threading.Thread(target=Algorithm)
+    algo_thread = threading.Thread(target=Algorithm, args=(robot_x, robot_y))
 
     rpi_thread.daemon = True
     algo_thread.daemon = True
