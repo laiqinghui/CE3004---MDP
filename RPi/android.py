66574786fc62a0bcc17737b0dfc37ee98a5521df
sohjunjie <junjie.soh93@gmail.com>
2018-02-02 10:12:15 +0800

    update bluetooth code

--- RPi/android.py
+++ RPi/android.py
@@ -10,41 +10,42 @@ import global_settings as gs
 
 class Android(threading.Thread):
 
-	def __init__(self):
-	    logging.info("android thread initialized")
-		# dispatcher.connect(self.  , signal=gs.ANDROID_SIGNAL, sender=ANDROID_SENDER)
+    def __init__(self):
+        logging.info("android thread initialized")
+        # dispatcher.connect(self.  , signal=gs.ANDROID_SIGNAL, sender=ANDROID_SENDER)
 
-		try:
+        try:
             self.server_socket = BluetoothSocket(RFCOMM)
-	    	self.server_socket.bind(("", PORT_ANY))
-	    	self.server_socket.listen(1)
-	    	self.port = self.server_socket.getsockname()[1]
-		
-	    	uuid = "4eed03ae-06c1-4aa5-862a-3c2712446594"
-    
-	    	advertise_service(self.server_socket, "MDPGroup5", service_id = uuid, service_classes = [uuid, SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE], protocols=[OBEX_UUID])
-            print("Waiting for Bluetooth connection ...")
+            self.server_socket.bind(("", PORT_ANY))
+            self.server_socket.listen(1)
+            self.port = self.server_socket.getsockname()[1]
+
+            uuid = "4eed03ae-06c1-4aa5-862a-3c2712446594"
+
+            advertise_service(self.server_socket, "MDPGroup5", service_id=uuid, service_classes=[uuid, SERIAL_PORT_CLASS], profiles=[SERIAL_PORT_PROFILE], protocols=[OBEX_UUID])
+            logging.info("Waiting for Bluetooth connection ...")
             self.client_sock, client_info = self.server_sock.accept()
-            print("Accepted Bluetooth connection.")
+            logging.info("Accepted Bluetooth connection.")
             while True:
                 data = client_sock.recv(1024)
-                if len(data) == 0: break
-                print("received [%s]" %data)
+                if len(data) == 0:
+                    break
+                logging.info("received [%s]" % data)
                 client_socket.send("a message from pi")
-        
+
         except:
             print "Error:"
-    
+
     # def sendAndroid(self, message):
     #     """
     #     Send a message to Android device
     #     """
-    #     try: 
+    #     try:
     #         self.client_socket.send(str(message))
     #     except BluetoothError:
     #         print "Bluetooth Error"
     #         self.connect_bluetooth()
-    
+
     # def receiveAndroid(self):
     #     """
     #     Receiving messages from Android
