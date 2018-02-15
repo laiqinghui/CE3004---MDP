af017efb16b2c95b8657a5f5201c0f1cc1d1f01e
sohjunjie <junjie.soh93@gmail.com>
2018-02-15 08:34:47 +0800

    change how android.py is initialised as thread"

--- RPi/android.py
+++ RPi/android.py
@@ -14,6 +14,8 @@ from robot.algorithm import Algorithm
 
 class Android(threading.Thread):
     def __init__(self):
+
+        super(Android, self).__init__()
         gs.init()
         logging.info("android thread initialized")
         dispatcher.connect(self.sendAndroid, signal=gs.RPI_ANDROID_SIGNAL, sender=gs.RPI_SENDER)
@@ -33,19 +35,7 @@ class Android(threading.Thread):
         # self.port = self.server_socket.getsockname()[1]
         self.server_socket.bind(("", self.port))
         self.server_socket.listen(1)
-        uuid = "00001101-0000-1000-8000-00805F9B34FB"
-
-        advertise_service(self.server_socket, "MDPGroup5", service_id=uuid, service_classes=[uuid, SERIAL_PORT_CLASS], profiles=[SERIAL_PORT_PROFILE])
-        logging.info("Waiting for Bluetooth connection on port " + str(self.port))
-        self.client_sock, self.client_info = self.server_socket.accept()
-        logging.info("Accepted connection from" + str(self.client_info))
-
-        try:
-            self.sendAndroid("Connection Secured")
-            while True:
-                self.receiveAndroid()
-        except IOError:
-            pass
+        self.uuid = "00001101-0000-1000-8000-00805F9B34FB"
 
     def startAlgorithm(self, robot_row, robot_col, waypoint_row, waypoint_col, goal_row, goal_col, mode, dir):
         self.algo_thread = Algorithm(robot_row, robot_col, waypoint_row, waypoint_col, goal_row, goal_col, mode, dir)
@@ -84,3 +74,26 @@ class Android(threading.Thread):
         self.client_sock.close()
         self.server_sock.close()
         logging.info("Bluetooth connection ended.")
+
+    def start(self):
+        self.running = True
+        super(Android, self).start()
+
+    def run(self):
+        advertise_service(self.server_socket, "MDPGroup5", service_id=self.uuid, service_classes=[self.uuid, SERIAL_PORT_CLASS], profiles=[SERIAL_PORT_PROFILE])
+        logging.info("Waiting for Bluetooth connection on port " + str(self.port))
+        self.client_sock, self.client_info = self.server_socket.accept()
+        logging.info("Accepted connection from" + str(self.client_info))
+
+        self.idle()
+
+    def stop(self):
+        self.running = False
+
+    def idle(self):
+        try:
+            self.sendAndroid("Connection Secured")
+            while(self.running):
+                self.receiveAndroid()
+        except IOError:
+            pass
