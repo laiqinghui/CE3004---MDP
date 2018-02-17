bcb37bfd1eb98edde3843a9717c41f422208e991
Lai Qing Hui <qinghui1994@gmail.com>
2018-02-17 17:38:36 +0800

    - PIDv2.2(Struct) now added in Main's Motor.h - Added gradual braking solution with low Gain - Added Sensors.h file which will house all sensors codes

---
+++ Arduino/main/Motor.h
@@ -0,0 +1,278 @@
+//For increase interrupt speed
+//#define NEEDFORSPEED
+//#define INTERRUPT_FLAG_PIN3 M1Ticks
+#include <EnableInterrupt.h>
+#include "DualVNH5019MotorShield.h"
+#include "Sensors.h"
+
+DualVNH5019MotorShield md(2,4,6,A0,7,8,12,A1);
+
+//------------Wheel Encoders constants------------
+#define e1a 3
+#define e1b 5
+#define e2a 11
+#define e2b 13
+
+//------------PID Structs------------
+struct MotorPID {
+  float prevTuneSpeed;
+  float currentErr;
+  float prevErr1;
+  float gain;
+};
+
+//------------Other constants and declrations----------
+#define Pi 3.1416
+#define singlerevticks 1124.43
+
+signed long wheelDiameter = 6*Pi;
+signed long ticksPerCM = singlerevticks/wheelDiameter;
+
+
+//------------Interrupt declarations------------
+volatile int squareWidth_M1 = 0;
+volatile signed long prev_time_M1 = 0;
+volatile signed long entry_time_M1 = 0;
+volatile unsigned long M1ticks = 0;
+
+
+volatile int squareWidth_M2 = 0;
+volatile signed long prev_time_M2 = 0;
+volatile signed long entry_time_M2 = 0; 
+
+
+//ISR for Motor1(Right) encoder 
+void risingM1()
+{
+  entry_time_M1 = micros();
+  squareWidth_M1 = entry_time_M1 - prev_time_M1;
+  prev_time_M1 = entry_time_M1;
+  M1ticks+=2;
+  
+  
+}
+
+//ISR for Motor2(Left) encoder
+void risingM2()
+{
+  entry_time_M2 = micros();
+  squareWidth_M2 = entry_time_M2 - prev_time_M2;
+  prev_time_M2 = entry_time_M2;
+  
+  
+}
+
+void setM1Ticks(int ticks){
+	M1ticks = ticks;
+}
+
+void setSqWidth(int M1, int M2){
+	squareWidth_M1 = M1;
+	squareWidth_M2 = M2;
+}
+
+signed long sqWidthToRPM(int sqWidth){
+
+  if(sqWidth <= 0)
+    return 0;
+  static double sqwavesPerRev = 562.25;
+  signed long sqwavesOneS = 1000000/sqWidth;//1/(sqWidth/1000000)
+  signed long sqwavesOneM = sqwavesOneS*60;
+  signed long revPerMin = sqwavesOneM/sqwavesPerRev;
+
+  return revPerMin;
+ 
+  }
+
+
+void tuneM1(int desiredRPM, MotorPID *M1){
+
+
+  double tuneSpeed = 0;
+  double currentRPM = sqWidthToRPM(squareWidth_M1);
+  
+  //Serial.print("M1 Current RPM: ");
+  Serial.print(currentRPM);
+  Serial.print(" ");
+
+ 
+  
+  
+  M1->currentErr =  desiredRPM - currentRPM;
+  //tuneSpeed = M1->prevTuneSpeed + 0.47*M1->currentErr;
+  tuneSpeed = M1->prevTuneSpeed + M1->gain*M1->currentErr + (M1->gain/0.05)*(M1->currentErr - M1->prevErr1);
+  /*
+  Serial.print("currentErr_M1 ");
+  Serial.println(currentErr_M1);
+  Serial.print("M1 tuneSpeed ");
+  Serial.println(tuneSpeed);
+  Serial.println();
+  */
+  md.setM1Speed(tuneSpeed);
+  M1->prevTuneSpeed = tuneSpeed;
+  M1->prevErr1 = M1->currentErr;
+
+ 
+  }
+
+  void tuneM2(int desiredRPM, MotorPID *M2){
+  
+  
+  double tuneSpeed = 0;
+  double currentRPM = sqWidthToRPM(squareWidth_M2);
+  
+  //Serial.print("M2 Current RPM: ");
+  Serial.println(currentRPM);
+
+  
+  
+  M2->currentErr =  desiredRPM - currentRPM;
+  //tuneSpeed = M2->prevTuneSpeed + 0.5*M2->currentErr;
+  tuneSpeed = M2->prevTuneSpeed + M2->gain*M2->currentErr + (M2->gain/0.05)*(M2->currentErr - M2->prevErr1);
+  /*
+  Serial.print("currentErr_M2 ");
+  Serial.println(currentErr_M2);
+  Serial.print("M2 tuneSpeed ");
+  Serial.println(tuneSpeed);
+  Serial.println();
+  */
+  md.setM2Speed(tuneSpeed);
+  M2->prevErr1 = M2->currentErr;
+  M2->prevTuneSpeed = tuneSpeed;
+  
+ 
+  }
+
+
+void moveForward(int rpm, int distance){
+
+   signed long tuneEntryTime = 0;
+   signed long tuneExitTime = 0;
+   signed long interval = 0;
+   signed long distanceTicks = distance * ticksPerCM;
+   int pidStartRPM = 0*rpm;
+   
+   //unsigned int rpm1 = 0;
+   //unsigned int rpm2 = 0;
+
+
+    
+    MotorPID M1pid = {184, 0, 0, 0.1};
+    MotorPID M2pid = {219, 0, 0, 0.1};
+    enableInterrupt( e1a, risingM1, RISING);
+    enableInterrupt( e2b, risingM2, RISING);
+
+    md.setM1Speed(184);
+    delay(1);
+    md.setM2Speed(219);
+
+    
+  
+
+    while(M1ticks < distanceTicks){
+      
+      //rpm1 = sqWidthToRPM(squareWidth_M1);
+      //rpm2 = sqWidthToRPM(squareWidth_M2);
+      
+      //Serial.print("M1 Current RPM: ");
+      //Serial.println(rpm1);
+      //Serial.print("\nM2 Current RPM: ");
+      //Serial.println(rpm2);
+      
+      if(sqWidthToRPM(squareWidth_M2) > pidStartRPM || sqWidthToRPM(squareWidth_M1) > pidStartRPM){
+      tuneEntryTime = micros();
+        interval = tuneEntryTime - tuneExitTime;
+        if(interval >= 5000){ 
+          if(M1ticks < 0.7*distanceTicks){
+            tuneM1(rpm, &M1pid);
+            tuneM2(rpm, &M2pid);
+          } else if(M1ticks < 0.85*distanceTicks){
+            tuneM1(rpm*0.75, &M1pid);
+            tuneM2(rpm*0.75, &M2pid);
+          } else{
+            tuneM1(rpm*0.5, &M1pid);
+            tuneM2(rpm*0.5, &M2pid);
+            }
+          tuneExitTime = micros();
+        }
+      }
+    }
+      md.setBrakes(400,400);
+      /*
+      delay(1000);
+      md.setSpeeds(-230.839, -265.47);
+      delay(7000);
+      md.setBrakes(400,400);
+      */
+      disableInterrupt(e1a);
+      disableInterrupt(e2b);
+      setM1Ticks(0);
+      setSqWidth(0,0);
+      }  
+/*
+void turn(int dir, int turnDegree)
+{
+    //1 is right, -1 is left
+    double cir = Pi * 17.5; //circumfrence of circle drawn when turning in cm, current diameter used is 17.6
+    int amount = abs(cir * (turnDegree/360.0) * ticksPerCM);//int to ignored decimal value //* getTurnTicksOffsetAmt(turnDegree)
+    
+    Serial.print("Target count: ");
+    Serial.println(amount);
+  
+	  enableInterruptFast(e1a, CHANGE);
+    M1Ticks = 0; 
+    md.setSpeeds(-159 * dir, 197 * dir);//80 RPM
+    
+    while(abs(M1Ticks) < amount - 500)
+    {
+    }
+    
+	  md.setSpeeds(-270 * dir, 300 * dir)
+	  while(abs(M1Ticks) < amount - 500)
+    {
+    }
+    
+
+    md.setBrakes(400,400);
+    Serial.print("Current amt: ");
+    Serial.println(M1Ticks);
+    
+	setSqWidth(0,0);//Reset sqWidth
+	M1Ticks = 0;
+	
+	disableInterrupt(e1a);
+}
+*/
+/*  
+void straightUsingEncoder()
+{
+	md.setSpeeds(270, 300);
+	PCintPort::attachInterrupt(e1a, &risingM1, RISING);
+	PCintPort::attachInterrupt(e2b, &risingM2, RISING);
+	
+	int m1Speed = 270;
+	
+	while(true)
+	{
+		setM1Ticks(0);
+		setM2Ticks(0);
+		delay(100);
+		
+		if(M1Ticks < M2Ticks)
+		{
+			m1Speed = m1Speed + 1;
+			md.setM1Speed(m1Speed);
+		}
+		else if(M1Ticks > M2Ticks)
+		{
+			m1Speed = m1Speed -1;
+			md.setM1Speed(m1Speed);
+		}
+		Serial.println("Next Update");
+		Serial.println(m1Speed);
+		Serial.println("300");
+		Serial.println(M1Ticks - M2Ticks);
+	}
+	
+} 
+*/
---
+++ Arduino/main/Sensors.h
@@ -0,0 +1,78 @@
+#define frontRightIR  A1    //Front right PS2
+#define frontLeftIR  A3    //Front left PS4
+#define left A2  // Left PS3
+#define right A0 // Right PS1
+
+int sensorsValuesArray[5] = {0};
+
+
+int USValue = 0;
+boolean flag=true;
+uint8_t DMcmd[4] = { 
+  0x22, 0x00, 0x00, 0x22}; //distance measure command
+  
+int getUltraSoundDistance(){
+
+	flag = true;
+  //Sending distance measure command :  0x22, 0x00, 0x00, 0x22 ;
+
+  for(int i=0;i<4;i++)
+  {
+    Serial.write(DMcmd[i]);
+  }
+
+  delay(40); //delay for 75 ms
+  unsigned long timer = millis();
+  while(millis() - timer < 30)
+  {
+    if(Serial.available()>0)
+    {
+      int header=Serial.read(); //0x22
+      int highbyte=Serial.read();
+      int lowbyte=Serial.read();
+      int sum=Serial.read();//sum
+
+      if(header == 0x22){
+        if(highbyte==255)
+        {
+          USValue=65525;  //if highbyte =255 , the reading is invalid.
+        }
+        else
+        {
+          USValue = highbyte*255+lowbyte;
+        }
+
+        Serial.print("Distance=");
+        Serial.println(USValue);
+
+        flag=false;
+      }
+      else{
+        while(Serial.available())  byte bufferClear = Serial.read();
+        break;
+      }
+    }
+  }
+  return USValue;
+
+
+}  
+
+int* getSensorReadingInCM(){//Quick and dirty test i.e no avg/median of sensor value
+  
+    //Return pointer to sensors values array
+		sensorsValuesArray[0] = 500; //getUltraSoundDistance();
+		sensorsValuesArray[1] = (6787/analogRead(frontLeftIR) - 3) - 4;
+		sensorsValuesArray[2] = (6787/analogRead(frontRightIR) - 3) - 4;
+		sensorsValuesArray[3] = 60.374 * pow( ( analogRead(left)*(5.0 / 1023.0) ) , -1.16);
+		sensorsValuesArray[4] = 60.374 * pow( ( analogRead(right)*(5.0 / 1023.0) ) , -1.16);
+					
+	  return sensorsValuesArray;
+	}
+
+int findMin(int arr[]) {
+    int min1Index = (arr[0] > arr[1]) ? 1:0;
+    int finalMinIndex = (arr[min1Index] > arr[2]) ? 2:min1Index;
+    return finalMinIndex;
+}
+
