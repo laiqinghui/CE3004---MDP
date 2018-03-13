0a5a5d35b91842cd2d23867f89242e4aa8d48d51
ben12385 <benjamin_chua@hotmail.com>
2018-03-13 19:21:06 +0800

    update

---
+++ Arduino/main/Turning.h
@@ -0,0 +1,142 @@
+#include "Constants.h"
+#include "PID.h"
+#include <EEPROM.h>
+//----------------------------------------------------------------TURNING----------------------------------------------------------------
+double getTurnValueOffset(int dir, int turnDegree);
+void setTurnValueOffset(int dir, int turnDegree, double newValue);
+double getTurnAmount(int dir, int turnDegree);
+void turnPID(int dir, int degree);
+
+double turnRight90Offset = 0;
+double turnLeft90Offset = 0;
+
+double getTurnValueOffset(int dir, int turnDegree) {
+  //Right Turn
+  if (dir == 1)
+  {
+	return turnRight90Offset;
+  }
+  //Left Turn
+  else
+  {
+    return turnLeft90Offset;
+  }
+}
+
+void setTurnValueOffset(int dir, int turnDegree, double newValue){
+  //Right Turn
+  if (dir == 1)
+  {
+		turnRight90Offset = newValue;
+    EEPROM.write(0, (newValue*100)>>8);
+    EEPROM.write(1, (newValue*100)&0xFF);
+  }
+  //Left Turn
+  else
+  {
+     turnLeft90Offset = newValue;
+     EEPROM.write(2, (newValue*100)>>8);
+     EEPROM.write(3, (newValue*100)&0xFF);
+  }
+}
+
+double getTurnAmount(int dir, int turnDegree) {
+  //Right Turn
+  if (dir == 1)
+  {
+    //180
+    if (turnDegree == 180)
+    {
+	    return abs(47.7 * 0.5 * ticksPerCM);  
+    }
+    //90
+    else
+    {
+		return abs(44.52 * (turnDegree / 360.0) * ticksPerCM) - turnRight90Offset;
+      
+    }
+  }
+  //Left Turn
+  else
+  {
+    return abs(43.25 * (turnDegree / 360.0) * ticksPerCM) - turnLeft90Offset;
+  }
+}
+
+int m1CurrentWidthPositive = 955;
+int m1CurrentWidthNegative = 975;
+
+void turnPID(int dir, int degree){
+	sideWall[0] = 0;
+	sideWall[1] = 0;
+	sideWall[2] = 0;
+	
+	int amount = getTurnAmount(dir, degree);
+	
+	int total = 0;
+	int currentTicksM1 = 0;
+	int currentM2Width = 0;
+	int difference = 0;
+	
+	enableInterrupt( e1a, risingM1Ticks, RISING);
+	enableInterrupt( e2b, risingM2, RISING);
+	setTicks(0, 0);
+	
+	if(dir == 1)
+	{
+		int m1Speed = -310;
+		int m2Speed = 330;
+		md.setSpeeds(m1Speed, m2Speed);
+		
+		while(currentTicksM1 < amount)
+		{
+			
+			noInterrupts();
+			currentTicksM1 = M1ticks;
+			currentM2Width = squareWidth_M2;
+			interrupts();
+     
+			if(currentM2Width > m1CurrentWidthNegative)
+			{
+				m2Speed = m2Speed - 1;
+				OCR1B = m2Speed;
+			}
+			else if(currentM2Width < m1CurrentWidthNegative)
+			{
+				m2Speed = m2Speed + 1;
+				OCR1B = m2Speed;
+			}
+		}
+	}
+	else
+	{
+		int m1Speed = 310;
+		int m2Speed = -320;
+		md.setSpeeds(m1Speed, m2Speed);
+
+		while(currentTicksM1 < amount)
+		{
+			noInterrupts();
+			currentTicksM1 = M1ticks;
+			currentM2Width = squareWidth_M2;
+			interrupts();
+     
+			if(currentM2Width < m1CurrentWidthPositive)
+			{
+				m2Speed = m2Speed - 1;
+				OCR1B = m2Speed;
+			}
+			else if(currentM2Width > m1CurrentWidthPositive)
+			{
+				m2Speed = m2Speed + 1;
+				OCR1B = m2Speed;
+			}
+		}
+	}
+	md.setBrakes(400, 400);
+
+	disableInterrupt(e1a);
+	disableInterrupt(e2b);
+	setTicks(0, 0);
+	setSqWidth(0,0);
+}
---
+++ Arduino/main/main.ino
@@ -0,0 +1,152 @@
+#include "I2C.h"
+#include "Forward.h"
+
+
+void benTestSequence()
+{
+  //straightenTune();
+  //moveForward(80, 30, true);  
+  //turnPID(-1, 90);
+  //md.setSpeeds(124, 170);
+  //while(true)
+  //{
+   //fastCalibration(2);
+  //}
+  //delay(1000);
+  //moveForward(80, 9.5, true);
+  //benForward(80, 9.5, true);
+  //moveForwardOneGrid(90);
+  //turnTest(-1, 90);
+  //test();
+}
+
+void processInst(){
+  
+  char *instBuff = getinBuffer();//inBuffer can be accessed directly but not a nice practice i think
+  int index = 1;//Start with 1 as first character is sensor flag which is checked after moving
+  char num[1] = {0};// For checklist
+  int moveCount = 0;
+  int delayAmount = 200;
+  
+  while(instBuff[index] != ';'){ 
+    switch(instBuff[index]){
+      
+      case 'W': moveCount = instBuff[index+1] - 48;
+                //delay(delayAmount);
+                if(moveCount == 1)
+                 moveForwardOneGrid(90);
+                else //moveForward(90, 9.5*moveCount, true);
+                     moveForwardBeta(90, 9.7*moveCount);
+                break;
+      case 'A': Serial.println('A');
+                moveCount = instBuff[index+1] - 48;
+                for(int a = 0; a < moveCount; a++){
+                  turnPID(-1, 90);
+                  delay(delayAmount);
+                }
+                //Reset side wall reading
+                resetSideWall();
+                break;
+      case 'D': moveCount = instBuff[index+1] - 48;
+                for(int a = 0; a < moveCount; a++){
+                  turnPID(1, 90);
+                  delay(delayAmount);
+                }
+                //Reset side wall reading
+                resetSideWall();
+                break;
+      case 'O': moveCount = instBuff[index+1] - 48;
+                for(int a = 0; a < moveCount; a++){
+                  turnPID(1, 90);
+                  delay(delayAmount);
+                  turnPID(1, 90);  
+                  delay(delayAmount);
+                }
+                //Reset side wall reading
+                resetSideWall();
+                break;
+      case ']': fastCalibration(2);
+                break;
+      case 'R': fastCalibration(1);
+                break;
+      case 'F': fastCalibration(0);
+                break;          
+      case '|': calibration();
+                break;                     
+      case 'T': turnPID(-1, atoi(instBuff+2));
+                break;
+      case 'S': setOutBuffer('S', getSensorReadingInCM(), 5);
+                interruptPi();//Interrupt RPI to notify data is ready 
+                break;
+      case 'I': num[0] = atoi(instBuff+2) +1;
+                Serial.print("num = ");
+                Serial.println(num[0]);
+                setOutBuffer('S', num, 1);
+                interruptPi();
+                break;
+      default:  ;//do nothing            
+      
+      }
+    
+    index+=2;
+    //if(instBuff[index] != ';')
+      //delay(200);
+    
+    }
+    //Serial.print("No. of move inst: ");
+    //Serial.println(index-1);
+    if(instBuff[0] == 'S'){
+  
+      setOutBuffer('S', getSensorReadingInCM(), 5);
+      printArray(outBuffer, 6);
+      interruptPi();//Interrupt RPI to notify data is ready 
+      }
+    
+
+}
+
+//---------------------------------------------------------------------------------------------
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Program Started!!!!");
+  md.init();
+  initI2C();
+
+  /*
+  EEPROM.write(0, 0);
+  EEPROM.write(1, 0);
+  EEPROM.write(2, 0);
+  */
+
+  int a = -200;
+
+   
+  //Load in the offset for turn
+  unsigned int turnRight90Offset1 = EEPROM.read(0);
+  unsigned int turnRight90Offset2 = EEPROM.read(1);
+  turnRight90Offset = ((signed int)((turnRight90Offset1 << 8) | turnRight90Offset2))100.0;
+
+  unsigned int turnLeft90Offset1 = EEPROM.read(2);
+  unsigned int turnLeft90Offset2 = EEPROM.read(3);
+  turnLeft90Offset = ((signed int)((turnLeft90Offset1 << 8) | turnLeft90Offset2))/100.0;  
+  
+  
+  //benTestSequence();
+}
+
+void loop() 
+{
+  //PWM_Mode_Setup();
+  //getUltraSound2Reading();
+  //Serial.println("Set");
+  //Serial.println(getIRSensorReading()[1]);
+    
+  if(dataExist()){
+    //delay(100);//Delay for ack packet to be sent out. To allow RPI to request and recieve data before we start moving which will affect interrupt operations 
+    processInst();
+  }
+    
+}
+
+
