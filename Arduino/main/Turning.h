74e7a8486e16940a0a6aa80664b55f44314e2fa7
ben12385 <benjamin_chua@hotmail.com>
2018-03-14 12:58:31 +0800

    Test

--- Arduino/main/Turning.h
+++ Arduino/main/Turning.h
@@ -68,43 +68,67 @@ int m1CurrentWidthNegative = 975;
 int m2TurnSpeedPositive = 330;
 int m2TurnSpeedNegative = -320;
 
-void turnPID(int dir, int degree) {
+void turnTest(int dir, int degree) {
+  
   sideWall[0] = 0;
   sideWall[1] = 0;
   sideWall[2] = 0;
-
+    
   int amount = getTurnAmount(dir, degree);
 
   int currentTicksM1 = 0;
+  int currentTicksM2 = 0;
   int currentM2Width = 0;
+  int currentTime = 0;
+  int prevTime = 0;
+  int prevM2Width = 0;
 
-  enableInterrupt( e1a, risingM1Ticks, RISING);
+  enableInterrupt( e1a, risingM1, RISING);
   enableInterrupt( e2b, risingM2, RISING);
   setTicks(0, 0);
-
+  
   if (dir == 1)
   {
     int m1Speed = -310;
     md.setSpeeds(m1Speed, m2TurnSpeedPositive);
-
+	
     while (currentTicksM1 < amount)
     {
 
       noInterrupts();
       currentTicksM1 = M1ticks;
-      currentM2Width = squareWidth_M2;
+	  currentTicksM2 = M2ticks;
       interrupts();
-
-      if (currentM2Width > m1CurrentWidthNegative)
-      {
-        m2TurnSpeedPositive = m2TurnSpeedPositive - 1;
-        OCR1B = m2TurnSpeedPositive;
-      }
-      else if (currentM2Width < m1CurrentWidthNegative)
-      {
-        m2TurnSpeedPositive = m2TurnSpeedPositive + 1;
-        OCR1B = m2TurnSpeedPositive;
-      }
+	  
+	  if(currentTicksM1 > currentTicksM2)
+	  {
+		  m2TurnSpeedPositive = m2TurnSpeedPositive + 1;
+	  }
+	  else if(currentTicksM1 < currentTicksM2)
+	  {
+		  m2TurnSpeedPositive = m2TurnSpeedPositive - 1;
+	  }
+	  
+	  /*
+	  currentTime = micros();
+	  if(currentTime-prevTime == 3000)
+	  {
+		  //Serial.println(m2TurnSpeedPositive);
+		  //Serial.println(currentM2Width);
+		  //Serial.println(m1CurrentWidthNegative);
+		  if (currentM2Width > m1CurrentWidthNegative)
+		  {
+			m2TurnSpeedPositive = m2TurnSpeedPositive + 1;
+			OCR1B = m2TurnSpeedPositive;
+		  }
+		  else if (currentM2Width < m1CurrentWidthNegative)
+		  {
+			m2TurnSpeedPositive = m2TurnSpeedPositive - 1;
+			OCR1B = m2TurnSpeedPositive;
+		  }
+		  prevTime = currentTime;
+	  }
+		*/	  
     }
   }
   else
@@ -114,21 +138,20 @@ void turnPID(int dir, int degree) {
 
     while (currentTicksM1 < amount)
     {
-      noInterrupts();
+	  noInterrupts();
       currentTicksM1 = M1ticks;
-      currentM2Width = squareWidth_M2;
+	  currentTicksM2 = M2ticks;
       interrupts();
-
-      if (currentM2Width < m1CurrentWidthPositive)
-      {
-        m2TurnSpeedNegative = m2TurnSpeedNegative - 1;
-        OCR1B = m2TurnSpeedNegative;
-      }
-      else if (currentM2Width > m1CurrentWidthPositive)
-      {
-        m2TurnSpeedNegative = m2TurnSpeedNegative + 1;
-        OCR1B = m2TurnSpeedNegative;
-      }
+	  
+	  if(currentTicksM1 > currentTicksM2)
+	  {
+		  m2TurnSpeedNegative = m2TurnSpeedNegative - 1;
+	  }
+	  else if(currentTicksM1 < currentTicksM2)
+	  {
+		  m2TurnSpeedNegative = m2TurnSpeedNegative + 1;
+	  }
+	  
     }
   }
   md.setBrakes(400, 400);
@@ -137,4 +160,104 @@ void turnPID(int dir, int degree) {
   disableInterrupt(e2b);
   setTicks(0, 0);
   setSqWidth(0, 0);
+
+}
+
+void turnPID(int dir, int degree) {
+  
+  sideWall[0] = 0;
+  sideWall[1] = 0;
+  sideWall[2] = 0;
+  
+  //Serial.println("Set");
+  //Serial.println(m2TurnSpeedNegative);
+  //Serial.println(m2TurnSpeedPositive);
+  
+  int amount = getTurnAmount(dir, degree);
+
+  int currentTicksM1 = 0;
+  int currentM1Width =0;
+  int currentM2Width = 0;
+  int currentTime = 0;
+  int prevTime = 0;
+  int prevM2Width = 0;
+
+  enableInterrupt( e1a, risingM1, RISING);
+  enableInterrupt( e2b, risingM2, RISING);
+  setTicks(0, 0);
+  
+  if (dir == 1)
+  {
+    md.setSpeeds(-310, m2TurnSpeedPositive);
+	
+    while (currentTicksM1 < amount)
+    {
+      noInterrupts();
+      currentTicksM1 = M1ticks;
+	  currentM1Width = squareWidth_M1;
+	  currentM2Width = squareWidth_M2;
+      interrupts();
+	  
+	  currentTime = micros();
+	  if(currentTime-prevTime == 1000)
+	  {
+		  //Serial.println(m2TurnSpeedPositive);
+		  //Serial.println(currentM2Width);
+		  //Serial.println(m1CurrentWidthNegative);
+		  if (currentM2Width > currentM1Width)
+		  {
+			m2TurnSpeedPositive = m2TurnSpeedPositive + 1;
+			OCR1B = m2TurnSpeedPositive;
+		  }
+		  else if (currentM2Width < currentM1Width)
+		  {
+			m2TurnSpeedPositive = m2TurnSpeedPositive - 1;
+			OCR1B = m2TurnSpeedPositive;
+		  }
+		  prevTime = currentTime;
+	  }
+    }
+  }
+  else
+  {
+    md.setSpeeds(310, m2TurnSpeedNegative);
+
+    while (currentTicksM1 < amount)
+    {
+      noInterrupts();
+      currentTicksM1 = M1ticks;
+	  currentM1Width = squareWidth_M1;
+	  currentM2Width = squareWidth_M2;
+      interrupts();
+	  
+	  currentTime = micros();
+	  if(currentTime-prevTime == 1000)
+	  {
+		  //Serial.println(m2TurnSpeedNegative);
+		  //Serial.println(currentM2Width);
+		  //Serial.println(m1CurrentWidthNegative);
+		  if (currentM2Width > currentM1Width)
+		  {
+			m2TurnSpeedNegative = m2TurnSpeedNegative - 1;
+			OCR1B = m2TurnSpeedNegative;
+		  }
+		  else if (currentM2Width < currentM1Width)
+		  {
+			m2TurnSpeedNegative = m2TurnSpeedNegative + 1;
+			OCR1B = m2TurnSpeedNegative;
+		  }
+		  prevTime = currentTime;
+	  }
+	  
+    }
+  }
+  md.setBrakes(400, 400);
+
+  disableInterrupt(e1a);
+  disableInterrupt(e2b);
+  setTicks(0, 0);
+  setSqWidth(0, 0);
+  
+  Serial.println(m2TurnSpeedPositive);
+  Serial.println(m2TurnSpeedNegative);
 }
