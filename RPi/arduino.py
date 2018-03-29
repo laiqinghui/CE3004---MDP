20348f3ec6867232c87fc36303f41baf17a0fa06
sohjunjie <junjie.soh93@gmail.com>
2018-03-29 13:56:21 +0800

    add read 2 more bytes

--- RPi/arduino.py
+++ RPi/arduino.py
@@ -62,7 +62,7 @@ class Arduino(threading.Thread):
         self.ser.write(packeted_instr[-1])
 
     def readData(self):
-        number = self.ser.read(6)
+        number = self.ser.read(8)
         return number
 
     def break_instruction_packets(self, instruction):
